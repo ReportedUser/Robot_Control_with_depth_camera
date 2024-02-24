38fc1d291f5537732826f6a27f541fb311d0f12f
ReportedUser <143010517+ReportedUser@users.noreply.github.com>
2024-02-24 15:29:12 +0100

    Working hand detector
    Need to change code so it's actually useful to UR and learn more about mediapipe and OpenCV.

--- opencv_to_universal_robot/scripts/camera_publisher.py
+++ opencv_to_universal_robot/scripts/camera_publisher.py
@@ -2,39 +2,45 @@
 
 import rospy
 import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 
+model_path="/home/david/Documents/mediapipe/handlandmarker/hand_landmarker.task"
+
 publisherNodeName='camera_sensor_publisher'
 
 topicName='video_topic'
-
 rospy.init_node(publisherNodeName, anonymous=True)
 publisher=rospy.Publisher(topicName, Image, queue_size=60)
 rate=rospy.Rate(15)
 
 
-hands = mp.solutions.hands
-drawing = mp.solutions.drawing_utils
+def print_result(result: vision.HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('hand landmarker result: {}'.format(result))
 
-video_capture_object=cv2.VideoCapture(0)
+
+video_capture_object=cv2.VideoCapture(2)
 bridge_object=CvBridge()
+base_options = python.BaseOptions(model_asset_path= model_path)
+options = vision.HandLandmarkerOptions(base_options = base_options, running_mode=vision.RunningMode.LIVE_STREAM, result_callback=print_result)
 
-while not rospy.is_shutdown():
-    returnValue, capturedFrame = video_capture_object.read()
-    capturedFrame = cv2.cvtColor(capturedFrame, cv2.COLOR_BGR2RGB)
-    captured_frame_with_hand = hands.Hands().process(capturedFrame)
-    capturedFrame = cv2.cvtColor(capturedFrame, cv2.COLOR_RGB2BGR)
 
-    if captured_frame_with_hand.multi_hand_landmarks:
-        for hand_recognized in captured_frame_with_hand.multi_hand_landmarks:
-            drawing.draw_landmarks(capturedFrame, hand_recognized, connections=hands.HAND_CONNECTIONS)
+with vision.HandLandmarker.create_from_options(options) as landmarker:
 
-    if returnValue == True:
-        rospy.loginfo('Video frame captured and published')
-        image_to_transmit = bridge_object.cv2_to_imgmsg(capturedFrame)
-        publisher.publish(image_to_transmit)
+    time_stamp = 0
+    while not rospy.is_shutdown():
+        time_stamp += 1
 
-    rate.sleep()
+        boolean_captured_frame, capturedFrame = video_capture_object.read()
+
+        capturedFrame = mp.Image(image_format=mp.ImageFormat.SRGB, data=capturedFrame)
+
+        landmarker.detect_async(capturedFrame, time_stamp)
+        if boolean_captured_frame == True:
+            rospy.loginfo('Video frame captured and published')
+            publisher.publish()
+        rate.sleep()
