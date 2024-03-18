95e0a61de296ff4ac99febebdfd0341768e9b961
ReportedUser <143010517+ReportedUser@users.noreply.github.com>
2024-03-18 19:33:02 +0100

    added hand_to_coordinates_mediapipe.py
    added hand_to_coordinates_mediapipe.py

--- opencv_to_universal_robot/scripts/movement_tests.py
+++ opencv_to_universal_robot/scripts/movement_tests.py
@@ -24,11 +24,21 @@ JOINT_NAMES = [
     "wrist_3_joint",
 ]
 
+JOINT_TRAJECTORY_CONTROLLERS = [
+    "scaled_pos_joint_traj_controller",
+    "scaled_vel_joint_traj_controller",
+    "pos_joint_traj_controller",
+    "vel_joint_traj_controller",
+    "forward_joint_traj_controller",
+]
+
 CARTESIAN_TRAJECTORY_CONTROLLERS = [
     "pose_based_cartesian_traj_controller",
     "joint_based_cartesian_traj_controller",
     "forward_cartesian_traj_controller",
 ]
+
+CONFLICTING_CONTROLLERS = ["joint_group_vel_controller", "twist_controller"]
 
 
 class TrajectoryClient:
@@ -46,7 +56,12 @@ class TrajectoryClient:
             rospy.logerr("Could not reach controller switch service. Msg {}".format(err))
             sys.exit(-1)
 
-        self.cartesian_trajectory_controller = CARTESIAN_TRAJECTORY_CONTROLLERS[1]
+        self.cartesian_trajectory_controller = CARTESIAN_TRAJECTORY_CONTROLLERS[0]
+        self.joint_trajectory_controller = JOINT_TRAJECTORY_CONTROLLERS[0]
+
+    def send_joint_trajectory(self):
+        self.switch_controller(self.joint_trajectory_controller)
+
 
     def send_cartesian_trajectory(self):
         self.switch_controller(self.cartesian_trajectory_controller)
@@ -64,17 +79,18 @@ class TrajectoryClient:
 
         pose_list = [
             geometry_msgs.msg.Pose(
-                geometry_msgs.msg.Vector3(0.4, -0.1, 0.4), geometry_msgs.msg.Quaternion(0,0,0,1)),
-            geometry_msgs.msg.Pose(
+                geometry_msgs.msg.Point(0.1, 0, 0), geometry_msgs.msg.Quaternion(0.5,0,0,0.5)),]
+        """    geometry_msgs.msg.Pose(
                 geometry_msgs.msg.Vector3(0.4, -0.1, 0.6), geometry_msgs.msg.Quaternion(0, 0, 0, 1)),
-            geometry_msgs.msg.Pose(
+        geometry_msgs.msg.Pose(
                 geometry_msgs.msg.Vector3(0.4, 0.3, 0.6), geometry_msgs.msg.Quaternion(0, 0, 0, 1)),
             geometry_msgs.msg.Pose(
-                geometry_msgs.msg.Vector3(0.4, 0.3, 0.4), geometry_msgs.msg.Quaternion(0, 0, 0, 1)),
+                geometry_msgs.msg.Vector3(0.4, 0.3, 0), geometry_msgs.msg.Quaternion(0, 0, 0, 1)),
             geometry_msgs.msg.Pose(
-                geometry_msgs.msg.Vector3(0.4, -0.1, 0.4), geometry_msgs.msg.Quaternion(0, 0, 0, 1)),
-        ]
-        duration_list = [3.0, 4.0, 5.0, 6.0, 7.0]
+                geometry_msgs.msg.Vector3(0.4, -0.1, 0), geometry_msgs.msg.Quaternion(0, 0, 0, 1)),
+        """
+
+        duration_list = [3, 4, 5, 6, 7]
         for i, pose in enumerate(pose_list):
             point = CartesianTrajectoryPoint()
             point.pose = pose
@@ -90,24 +106,10 @@ class TrajectoryClient:
 
         rospy.loginfo("Trajectory execution finished in state {}".format(result.error_code))
 
-    def ask_confirmation(self, waypoint_list):
-        rospy.logwarn("The robot will move to the following waypoints: \n{}".format(waypoint_list))
-        confirmed = False
-        valid = False
-        while not valid:
-            input_str = input("Please confirm. Fill whenever It's not a simulation.")
-            valid = input_str in ["y", "n"]
-            if not valid:
-                rospy.loginfo("Please confirm by entering 'y' or abort by entering 'n'")
-            else:
-                confirmed = input_str == 'y'
-
-        if not confirmed:
-            rospy.loginfo("Existing as requested by user.")
-            sys.exit(0)
 
     def switch_controller(self, target_controller):
-        other_controllers = (CARTESIAN_TRAJECTORY_CONTROLLERS)
+        other_controllers = (JOINT_TRAJECTORY_CONTROLLERS + CARTESIAN_TRAJECTORY_CONTROLLERS + CONFLICTING_CONTROLLERS)
+        other_controllers.remove(target_controller)
 
         srv = ListControllersRequest()
         response = self.list_srv(srv)
